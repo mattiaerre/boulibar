691379d927733e4f645b3ee423dbd0e850328d86
mattia richetto <mattia.richetto@gmail.com>
2018-05-28 00:25:38 -0700

    add triangle

--- src/public/javascripts/index.js
+++ src/public/javascripts/index.js
@@ -1,6 +1,37 @@
-(document => {
+function addClickOnBurger() {
   const burger = document.getElementsByClassName('navbar-burger')[0];
   burger.addEventListener('click', () => {
     burger.classList.toggle('is-active');
   });
-})(document);
+}
+
+function addResizeOnWindow(handler) {
+  window.addEventListener('resize', handler);
+}
+
+function addTriangleAboveHeroImage() {
+  const { clientWidth } = document.body;
+  const image = document.getElementById('hero-image');
+  const triangle = {
+    height: clientWidth / 6,
+    width: clientWidth
+  };
+  const sheet = document.styleSheets[0];
+  sheet.insertRule(
+    `.hero-image {
+        border-bottom: ${triangle.height}px solid #363636;
+        border-right: ${triangle.width}px solid transparent;
+        height: 0;
+        position: absolute;
+        top: ${image.height - triangle.height}px;
+        width: 0;
+    }`,
+    sheet.cssRules.length
+  );
+}
+
+(() => {
+  addClickOnBurger();
+  addResizeOnWindow(addTriangleAboveHeroImage);
+  addTriangleAboveHeroImage();
+})();
