3e8b93ca77903ac60a18d667e71550d5ee683fd5
mattia richetto <mattia.richetto@gmail.com>
2018-05-28 20:54:50 -0700

    navigation

--- src/public/javascripts/index.js
+++ src/public/javascripts/index.js
@@ -1,37 +1,17 @@
-function addClickOnBurger() {
-  const burger = document.getElementsByClassName('navbar-burger')[0];
-  burger.addEventListener('click', () => {
-    burger.classList.toggle('is-active');
-  });
-}
-
-function addResizeOnWindow(handler) {
-  window.addEventListener('resize', handler);
-}
-
-function addTriangleAboveHeroImage() {
-  const { width } = window.screen;
-  const image = document.getElementById('hero-image');
-  const triangle = {
-    height: width / 6,
-    width
-  };
-  const sheet = document.styleSheets[0];
-  sheet.insertRule(
-    `.hero-image {
-        border-bottom: ${triangle.height}px solid #363636;
-        border-right: ${triangle.width}px solid transparent;
-        height: 0;
-        position: absolute;
-        top: ${image.height - triangle.height}px;
-        width: 0;
-    }`,
-    sheet.cssRules.length
+document.addEventListener('DOMContentLoaded', () => {
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll('.navbar-burger'),
+    0
   );
-}
 
-(() => {
-  addClickOnBurger();
-  addResizeOnWindow(addTriangleAboveHeroImage);
-  addTriangleAboveHeroImage();
-})();
+  if ($navbarBurgers.length > 0) {
+    $navbarBurgers.forEach($el => {
+      $el.addEventListener('click', () => {
+        const { target } = $el.dataset;
+        const $target = document.getElementById(target);
+        $el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      });
+    });
+  }
+});
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,19 +2,29 @@ const debug = require('debug')('boulibar:routes/index');
 const express = require('express');
 const { name, version } = require('../../package.json');
 const copy = require('./copy.json');
+const siteMap = require('./site-map.json');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+function handler(req, res, page) {
   const stylesheet = `${process.env.PROD === 'true' ? 'stylesheets' : ''}`;
   const model = {
     copy,
-    name: name.toUpperCase(),
+    name,
+    page,
+    path: req.path.substring(1) || 'home',
     stylesheet,
     version
   };
   debug(model);
   res.render('index', model);
-});
+}
+
+Object.keys(siteMap)
+  .map(key => siteMap[key])
+  .filter(page => page.v1 === true)
+  .forEach(page => {
+    router.get(page.path, (req, res) => handler(req, res, page));
+  });
 
 module.exports = router;
